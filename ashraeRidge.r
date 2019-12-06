ed83d578d59c94ba4f0d9952c1bed8a7967ad89b
adam <adam32122@gmail.com>
2019-12-06 17:38:17 +0000

    updated tester

--- ashraeRidge.r
+++ ashraeRidge.r
@@ -54,44 +54,35 @@ kfoldVanillaTest <- function(daily_dataDate,k=8){
  
 }
 
-kfoldRidgeTest <- function(daily_dataDate,k=8){
+kfoldRidgeTest <- function(daily_dataDate,k=8,lambda=0.0){
   #kfold tester for the ridge regression
   #tests all lambdas between 0 & 10 at a .2 increment
   #returns a ggplot of the mse's by lambda
   sliceSize <- nrow(daily_dataDate)/k
-  lambdas = c()
-  mses = c()
-  for(lambda in seq(0,10,0.2)){
-    mse <- 0
-    for(i in seq(0,k-1)){
-      
-      for(mtype in daily_dataDate %>% distinct(meter) %>% pull(meter)){
-        testStart <- i*sliceSize + 1
-        testEnd <- (i+1)*sliceSize
-        testDf <- daily_dataDate %>% slice(testStart:testEnd)
-        testDf <- testDf
-        trainDfHead <- daily_dataDate %>% slice(0:max(testStart-1,0))
-        trainDfTail <- daily_dataDate %>% slice(testEnd+1:nrow(daily_dataDate))
-        if(nrow(trainDfHead) == 0){
-          trainDf <- trainDfTail
-        }else if(nrow(trainDfTail)==0){
-          trainDf <- trainDfHead
-        }else{
-          trainDf<-rbind(trainDfHead,trainDfTail)
-        }
-        trainDf <- trainDf %>% filter(meter==mtype)
-        testDf <- testDf %>% filter(meter==mtype)
-        mod <- buildRidgeModel(trainDf,lambda = lambda)
-        mse <- testModel(testDf,mod)+ mse
+  mse <- 0
+  for(i in seq(0,k-1)){
+    
+    for(mtype in daily_dataDate %>% distinct(meter) %>% pull(meter)){
+      testStart <- i*sliceSize + 1
+      testEnd <- (i+1)*sliceSize
+      testDf <- daily_dataDate %>% slice(testStart:testEnd)
+      testDf <- testDf
+      trainDfHead <- daily_dataDate %>% slice(0:max(testStart-1,0))
+      trainDfTail <- daily_dataDate %>% slice(testEnd+1:nrow(daily_dataDate))
+      if(nrow(trainDfHead) == 0){
+        trainDf <- trainDfTail
+      }else if(nrow(trainDfTail)==0){
+        trainDf <- trainDfHead
+      }else{
+        trainDf<-rbind(trainDfHead,trainDfTail)
       }
+      trainDf <- trainDf %>% filter(meter==mtype)
+      testDf <- testDf %>% filter(meter==mtype)
+      mod <- buildRidgeModel(trainDf,lambda = lambda)
+      mse <- testModel(testDf,mod)+ mse
     }
-    mse <- (mse/k)/daily_dataDate %>% distinct(meter) %>% nrow()
-    lambdas <- append(lambdas,lambda)
-    mses <- append(mses,mse)
   }
-  ggplot(data=NULL,aes(x=lambdas,y=mses))+
-    geom_line()+
-    geom_point()
-  
+  mse <- (mse/k)/daily_dataDate %>% distinct(meter) %>% nrow()
+  mse
   
 }
--- tester.r
+++ tester.r
@@ -5,6 +5,17 @@ daily_data <- daily_data %>% ungroup()
 
 daily_dataDate <- addDayOfYear(daily_dataDate)
 
-kfoldRidgeTest(daily_dataDate,k=12)
-vanmse <-kfoldVanillaTest(daily_dataDate)
 
+lambdas = c()
+mses = c()
+for(lambda in seq(0,5,.1)){
+  rmse <- kfoldRidgeTest(daily_dataDate,k=12,lambda)
+  mses <- append(rmse,mses)
+  lambdas <- append(lambda,lambdas)
+}
+vanmse <-kfoldVanillaTest(daily_dataDate)
+print(vanmse)
+ggplot(data=NULL, aes(x=lambdas,y=mses))+
+  geom_point()+
+  geom_line()+
+  geom_point(data=NULL,aes(x=c(0),y=c(vanmse)),color='red')
